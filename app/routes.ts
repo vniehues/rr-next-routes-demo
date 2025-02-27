c78aea6f6b2ae6d70b487be2ad5091a31cee1f41
Vincent Niehues <vniehues@icloud.com>
2025-02-27 23:02:57 +0100

    route structure

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,3 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {nextRoutes, pageRouterStyle} from "rr-next-routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default nextRoutes({...pageRouterStyle, print: "table", folderName: "routes", layoutFileName: "layout"});
